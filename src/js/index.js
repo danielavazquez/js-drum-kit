2f89d4001e2a6efffe2965f2468e50f864fdcbbe
Daniela Vazquez <daniela_056@hotmail.com>
2019-11-21 15:15:22 -0600

    Adding data attributes, css styling, wav files, and event listerners

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
-window.addEventListener("keydown", function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-  const audio = document.querySelector(`.key[data-key="${e.keycode}"]`); //for are only looking for one keycode if it were many then querySelectorAll. Also using a data-attribute selector
-  if (!audio) return; //stop the function from running all together
-  audio.currentTime = 0; //rewind to the start
-  audio.play();
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
   key.classList.add("playing");
-});
+  audio.currentTime = 0;
+  audio.play();
+}
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
