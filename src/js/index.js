6c8d3ff2de380ad51fa2a09de3535441286237ed
Daniela Vazquez <daniela_056@hotmail.com>
2019-11-21 16:41:37 -0600

    css to scss changes

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,9 @@ function removeTransition(e) {
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
+  if (!audio) return; //stop the function from running all together
   key.classList.add("playing");
-  audio.currentTime = 0;
+  audio.currentTime = 0; //rewind to the start
   audio.play();
 }
 const keys = Array.from(document.querySelectorAll(".key"));
